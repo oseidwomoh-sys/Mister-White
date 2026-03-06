3d3a2ad83eaa52b1479e09fdbaff73de4c70612b
Hendrix Tavarez <htavarez@gmail.com>
2026-03-05 21:12:49 -0500

    mar 5

---
+++ java/networking/App.java
@@ -0,0 +1,26 @@
+// add package name here if needed
+//
+class App {
+
+  public static void main(String[] args) {
+    // IPv4 address
+    // 32 bits
+    // 4 octets
+    // 192.168.1.100
+    // each octet is 8 bits
+    // Using 8 bits, we can represent numbers from 0 to 255
+    //
+    // Number of addresses in IPv4: 2^32 = 4,294,967,296
+    //
+    // IPv6 address
+    // 128 bits
+    // 8 groups of 16 bits
+    // 2001:0db8:85a3:0000:0000:8a2e:0370:7334
+    // each group is 16 bits, which can represent numbers from 0 to 65535
+    // octets are represented in hexadecimal format
+    //
+    // Number of addresses in IPv6: 2^128 = 340,282,366,920,938,463,463,374,607,431,768,211,456
+   
+  }
+}
+
